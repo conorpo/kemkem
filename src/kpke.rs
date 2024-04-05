4e135c52f474c96817547bf3519ac26d4eeb97d3
Conor O'Malley <conor4arms@gmail.com>
2024-04-05 17:00:00 -0400

    last change

--- src/kpke.rs
+++ src/kpke.rs
@@ -51,3 +51,41 @@ pub fn KeyGen<const k: usize>() -> (Matrix<Polynomial>, Vector<Polynomial>, Vect
     todo!() // byte encode (t)||p and s   
     //(t||p) is the encapsulation key, s is the secret (decapsulation) key
 }
+
+pub fn Encrypt<const k: usize>(ek_pke, m: [u8; 32], r: [u8; 32]) -> ([u8; 32], [u8; 128]) {
+    let (rho, sigma) = crypt::G(ek_pke);
+
+    let mut N = 0;
+
+    let mut u: Vec<k> = Vec::new(PolynomialType::Ring);
+    for i in 0..params.k {
+        u.push( match params.eta_1 {
+            2 => SamplePolyCBD_2(PRF_2(sigma, N)),
+            3 => SamplePolyCBD_3(PRF_3(sigma, N)),
+            _ => panic!("Invalid eta_1")
+        });
+        N += 1;
+    }
+
+    let mut v: Vec<k> = Vec::new(PolynomialType::Ring);
+    for i in 0..params.k {
+        v.push( match params.eta_2 {
+            2 => SamplePolyCBD_2(PRF_2(sigma, N)),
+            3 => SamplePolyCBD_3(PRF_3(sigma, N)),
+            _ => panic!("Invalid eta_2")
+        });
+        N += 1;
+    }
+
+    let u = u.NTT();
+    let v = v.NTT();
+
+    let c = ek_pke.vector_multiply(&u).add(&v);
+
+    let t = crypt::H(c);
+    let k = crypt::J(c);
+
+    let c = crypt::prf_2(t, 0);
+
+    (k, c)
+}
